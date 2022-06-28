0d5144437c82aa41f7f8f11fda8610072f6049ed
Michelle Ramirez <micheller@nvidia.com>
2022-06-28 21:44:00 +0000

    added preprocessing func skeletons

---
+++ classification/data_preprocessing.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+
+
+def get_video_frames():
+    '''Function to get frames from .mp4 video files'''
+
+def pad_videos():
+    
+    '''Function to pad videos that fall short of standardized frame count by replicating middle frame'''
+    
