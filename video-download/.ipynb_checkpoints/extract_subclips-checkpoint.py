f151a87842715a23e2fb9b1941db716d31e9621a
Michelle Ramirez <mramir71@uic.edu>
2022-06-30 20:57:13 +0000

    added code to extract clips into wkspace

--- video-download/.ipynb_checkpoints/extract_subclips-checkpoint.py
+++ video-download/.ipynb_checkpoints/extract_subclips-checkpoint.py
@@ -1,4 +1,16 @@
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+import pandas as pd
 
+''' File to get clips at start, stop intervals from full length videos from dataset '''
 
-# ffmpeg_extract_subclip(full_video, start_seconds, end_seconds, targetname=video_clip_name)
+WORKSPACE_PATH = "/mount/data/"
+downloads_df = pd.read_csv(WORKSPACE_PATH + '/downloaded_videos.csv')
+for i, row in downloads_df[0:2].iterrows():
+    
+    full_video = WORKSPACE_PATH + 'videos/' + row['renamed_title']
+    video_clip_name = row['renamed_title'].replace('_', '_clip_')
+
+    #download clip at start, stop interval
+    ffmpeg_extract_subclip(full_video, row['clip_start'], row['clip_end'], targetname=WORKSPACE_PATH + 'video_clips/' + video_clip_name)
+    
+print('Done downloading video clips.')
