41966da7bb5f5f285389f76d6cfb14ed868edf9f
Michelle Ramirez <mramir71@uic.edu>
2022-07-01 18:43:22 +0000

    video frame capture edits

--- classification/.ipynb_checkpoints/data_preprocessing-checkpoint.py
+++ classification/.ipynb_checkpoints/data_preprocessing-checkpoint.py
@@ -5,40 +5,34 @@ workspace_path = '/mount/data'
 
 def get_video_frames(video_title, max_frames=500, resize=(224, 224)):
     '''Function to get frames from .mp4 video files'''
-    
-    print(max_frames) 
-    
-    count = 0
-    num_frames = 0
+        
+    num_frames_extracted = 0
+    frames = []
 
     cap = cv2.VideoCapture(workspace_path + '/video_clips/' + video_title)
-    frames = []
-    
-    print(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    num_total_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
     fps = int(cap.get(cv2.CAP_PROP_FPS))
-    print(fps)
-
-    try:
-        while True and num_frames < max_frames:
-            ret, frame = cap.read()
-            if not ret:
+    
+    success = True
+    next_frame_index = 0
+    if num_total_frames <= max_frames:
+        #collect every frame available
+        while success:
+            success, frame = cap.read()
+            if not success:
                 break
 
             frame = cv2.resize(frame, resize)
             frame = frame[:, :, [2, 1, 0]]  #reorders color channels from BGR [0,1,2] to RGB order [2,1,0]
             frames.append(frame)
+            num_frames_extracted += 1 
 
-            #to skip over some redundant frames and get more diverse set of frames over span of our video
-            #while still getting a fixed, uniform number of max_frames for each video
-            count += 0.5 * fps
-            cap.set(1, count)
+            next_frame_index += 1
+            cap.set(cv2.CAP_PROP_POS_FRAMES, next_frame_index)
 
-            num_frames += 1
-
-    finally:
         cap.release()
 
-    print(f'num frames {num_frames}')
+    print(f'num frames extracted: {num_frames_extracted}')
     return np.array(frames)
 
 # def pad_videos():
