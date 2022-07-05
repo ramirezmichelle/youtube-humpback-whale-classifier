5e0fb98bb59dc2573eefe62aabd8bce5703ded1c
Michelle Ramirez <mramir71@uic.edu>
2022-07-05 17:38:00 +0000

    configuring code to extract frames

--- classification/.ipynb_checkpoints/data_preprocessing-checkpoint.py
+++ classification/.ipynb_checkpoints/data_preprocessing-checkpoint.py
@@ -3,37 +3,40 @@ import numpy as np
 
 workspace_path = '/mount/data'
 
-def get_video_frames(video_title, max_frames=500, resize=(224, 224)):
+def get_video_frames(video, resize=(224, 224)):
     '''Function to get frames from .mp4 video files'''
         
-    num_frames_extracted = 0
-    frames = []
-
-    cap = cv2.VideoCapture(workspace_path + '/video_clips/' + video_title)
-    num_total_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
-    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    clip_number = video.split('_')[2].split('.')[0]
+    cap = cv2.VideoCapture(workspace_path + "/video_clips/" + video)
     
+    #store how many frames are reported to exist in video's metadata
+    total_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+        
+    #keep track of frame count
+    count = 0
+    
+    #check whether frames were extracted
     success = True
-    next_frame_index = 0
-    if num_total_frames <= max_frames:
-        #collect every frame available
-        while success:
-            success, frame = cap.read()
-            if not success:
-                break
+    
+    #collect every frame available
+    while success:
+        
+        success, frame = cap.read()
+        
+        if not success:
+            break
 
-            frame = cv2.resize(frame, resize)
-            frame = frame[:, :, [2, 1, 0]]  #reorders color channels from BGR [0,1,2] to RGB order [2,1,0]
-            frames.append(frame)
-            num_frames_extracted += 1 
+        #resize frame to 224 x 224 and reorder color channels
+        frame = cv2.resize(frame, resize)
+        frame = frame[:, :, [2, 1, 0]]  
 
-            next_frame_index += 1
-            cap.set(cv2.CAP_PROP_POS_FRAMES, next_frame_index)
+        #save frame as jpeg file
+        cv2.imwrite(workspace_path + "/frames/" + "/clip_%s_frame_%d.jpg" % (clip_number, count), frame)             
+        count += 1
 
-        cap.release()
+    cap.release()
 
-    print(f'num frames extracted: {num_frames_extracted}')
-    return np.array(frames)
+    return count, total_frames
 
 
 def frame_capture(path):
