6ea866555ea3ea4f4256e865ee82ce2c424a1d5e
Michelle Ramirez <mramir71@uic.edu>
2022-07-06 13:09:30 +0000

    removed opencv-based functions for cleanup

--- classification/.ipynb_checkpoints/data_preprocessing-checkpoint.py
+++ classification/.ipynb_checkpoints/data_preprocessing-checkpoint.py
@@ -6,42 +6,6 @@ import matplotlib.pyplot as plt
 
 workspace_path = '/mount/data'
 
-def get_video_frames_opencv(video, resize=(224, 224)):
-    '''Function to get frames from .mp4 video files'''
-        
-    clip_number = video.split('_')[2].split('.')[0]
-    cap = cv2.VideoCapture(workspace_path + "/video_clips/" + video)
-    
-    #store how many frames are reported to exist in video's metadata
-    total_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
-        
-    #keep track of frame count
-    count = 0
-    
-    #check whether frames were extracted
-    success = True
-    
-    #collect every frame available
-    while success:
-        
-        success, frame = cap.read()
-        
-        if not success:
-            break
-
-        #resize frame to 224 x 224 and reorder color channels
-        frame = cv2.resize(frame, resize)
-        frame = frame[:, :, [2, 1, 0]]  
-
-        #save frame as jpeg file
-        cv2.imwrite(workspace_path + "/frames/" + "/clip_%s_frame_%d.jpg" % (clip_number, count), frame)             
-        count += 1
-
-    cap.release()
-
-    return count, total_frames
-
-
 def get_video_frames(video_title, max_frames, context=cpu(0), resize=(224,224)):
     ''' Get individual image frames from video '''
     
@@ -54,12 +18,16 @@ def get_video_frames(video_title, max_frames, context=cpu(0), resize=(224,224)):
     #get batch of frames that matches amount needed
     frames = get_n_frames(vr, len(vr))
 
-#     #save frames as jpg images 
-#     for i in range(len(frames)):
-#         frame = frames[i].asnumpy()
-#         frame = cv2.resize(frame, resize)
-#         plt.imsave(workspace_path + "/frames/" + "/clip_%s_frame_%d.jpg" % (clip_number, i), frame)
-    
+    #save frames as jpg images 
+    for i in range(len(frames)):
+        frame = frames[i].asnumpy()
+        frame = cv2.resize(frame, resize)
+        
+        #reorder color channels (will leave out for now)
+        #frame = frame[:, :, [2, 1, 0]] 
+        
+        #save frame image in directory
+        plt.imsave(workspace_path + "/frames/" + "/clip_%s_frame_%d.jpg" % (clip_number, i), frame)
     
     return len(frames), len(vr)
     
