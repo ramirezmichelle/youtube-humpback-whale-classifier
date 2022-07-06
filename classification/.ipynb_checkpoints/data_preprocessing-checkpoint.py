8b21db2c36f845867fa35dc79b1c2bb18ba4351e
Michelle Ramirez <mramir71@uic.edu>
2022-07-06 01:59:19 +0000

    adding functions to extract frames with decord

--- classification/.ipynb_checkpoints/data_preprocessing-checkpoint.py
+++ classification/.ipynb_checkpoints/data_preprocessing-checkpoint.py
@@ -1,9 +1,12 @@
 import cv2
 import numpy as np
+from decord import VideoReader
+from decord import cpu, gpu
+import matplotlib.pyplot as plt
 
 workspace_path = '/mount/data'
 
-def get_video_frames(video, resize=(224, 224)):
+def get_video_frames_opencv(video, resize=(224, 224)):
     '''Function to get frames from .mp4 video files'''
         
     clip_number = video.split('_')[2].split('.')[0]
@@ -39,33 +42,71 @@ def get_video_frames(video, resize=(224, 224)):
     return count, total_frames
 
 
-def frame_capture(path):
+def get_video_frames(video_title, max_frames, context=cpu(0), resize=(224,224)):
+    ''' Get individual image frames from video '''
     
-    vidObj = cv2.VideoCapture(path)
-    total_frames = vidObj.get(cv2.CAP_PROP_FRAME_COUNT)
-    print(total_frames)
+    #get clip number for frame naming
+    clip_number = video_title.split('_')[2].split('.')[0]
     
-    #keep track of frame count
-    count = 0
+    #read video
+    vr = VideoReader(workspace_path + '/video_clips/' + video_title, ctx=context)
     
-    #check whether frames were extracted
-    success = 1
+    #get batch of frames that matches amount needed
+    frames = get_n_frames(vr, len(vr))
+
+    #save frames as jpg images 
+    for i in range(len(frames)):
+        frame = frames[i].asnumpy()
+        frame = cv2.resize(frame, resize)
+        plt.imsave(workspace_path + "/frames/" + "/clip_%s_frame_%d.jpg" % (clip_number, i), frame)
     
-    while count != total_frames:
-        success, image = vidObj.read()
+    
+    return len(frames), len(vr)
+    
+
+def get_n_frames(video_reader, max_frames):
+    ''' Get the needed number of frames (max_frames)'''
+    
+    if len(video_reader) > max_frames:
+        #only sample the needed amt of frames
+        frame_indices = sample_n_frames(len(video_reader), max_frames)
+        frames = video_reader.get_batch(frame_indices)
+    
+    elif len(video_reader) < max_frames:
+        #pad frames
+        frame_indices = pad_frames(len(video_reader), max_frames)
+        frames = video_reader.get_batch(frame_indices)
+   
+    else:
+        frames = video_reader
         
-        if success:
-            cv2.imwrite("frames/frame%d.jpg" % count, image)
-            count +=1
-        else:
-            print('frame not available')
-            break
-                
-    return count
+    return frames
     
-# def pad_videos():
+
+def sample_n_frames(num_available, n):
+    ''' Function to pick out n frames from total frames available
+    Inputs
+        num_available (int): number of frames available
+        n (int): number of frames we need
     
-#     '''Function to pad videos that fall short of standardized frame count by replicating middle frame'''
+    Outputs
+        frame_indices (int): indices of frames, including padded middle frame 
+    '''
+    return
+    
+    
+def pad_frames(num_available, n):
+    
+    '''Function to pad videos that fall short of standardized frame count by replicating middle frame.
+    Inputs
+        num_available (int): number of frames available
+        n (int): number of frames we need
+    
+    Outputs
+        frame_indices (int): indices of frames, including padded middle frame 
+    '''
+    return
+    
     
 # def standardize_videos():
     
