b7a493989cd62644397f62bc5a11165b2d7f6a11
Michelle Ramirez <micheller@nvidia.com>
2022-08-14 00:08:06 +0000

    adding wandb vis

--- classification/scripts/main.py
+++ classification/scripts/main.py
@@ -37,6 +37,68 @@ def get_F1_score(test_dataset, model):
 
     return metrics.f1_score(y_true, y_pred)
 
+# def get_predicted_labels(model, X_test, y_test):
+#     "Generate predicted probabilites and get predicted class (0 or 1). \
+#     Relevant class is denoted by label = 1, and Irrelevant class by label 0."
+    
+#     probabilities = model.predict(X_test)
+#     y_pred = np.array([np.argmax(p) for p in probabilities])
+#     y_true = y_test.flatten()
+    
+#     return y_pred, y_true
+
+# def get_test_results(X, y, test_index):
+#     test_results = pd.DataFrame({'File': X.loc[test_index].renamed_title.tolist(), 
+#                                  'True Relevant Label': y.loc[test_index].tolist(),
+#                                  'Pred Relevant Label': list(map(bool, y_pred))})
+
+
+# def get_false_positives():
+#     # False Positives (True Label = 0 ; Pred Label = 1)
+#     false_positives = test_results[(test_results["True Relevant Label"] == False) & (test_results["Pred Relevant Label"] == True)].copy(deep=True)
+
+#     clip_folder = workspace_path + "/video_clips/"
+
+#     #get wandb video objects
+#     for i, row in false_positives.iterrows():
+#         video_clip = clip_folder + row['File'].replace('_', '_clip_')
+#         false_positives.at[i, ('video_clip')] = wandb.Video(video_clip, fps=4, format="gif")
+
+#     #create wandb table with all information
+#     table = wandb.Table(dataframe=false_positives)
+#     wandb.log({"Videos Mistaken as Humpback Whale Encounters (False Positives)": table})
+
+# def get_false_negatives():
+#     # False Negatives (True Label = 1 ; Pred Label = 0)
+#     false_negatives = test_results[(test_results["True Relevant Label"] == True) & (test_results["Pred Relevant Label"] == False)].copy(deep = True)
+
+#     clip_folder = workspace_path + "/video_clips/"
+
+#     #get wandb video objects
+#     for i, row in false_negatives.iterrows():
+#         video_clip = clip_folder + row['File'].replace('_', '_clip_')
+#         false_negatives.at[i, ('video_clip')] = wandb.Video(video_clip, fps=4, format="gif")
+
+#     #create wandb table with all information
+#     table = wandb.Table(dataframe=false_negatives)
+#     wandb.log({"Videos Mistaken as NOT Having Humpback Whales (False Negatives)": table})
+
+# def display_model_train_wandb():
+#     #log training and validation metrics on wandb
+#     for epoch, train_loss in enumerate(history.history['loss']):
+#         wandb.log({'training_loss': train_loss, "epoch": epoch})
+
+#     for epoch, train_acc in enumerate(history.history['accuracy']):
+#         wandb.log({'training_accuracy': train_acc, "epoch": epoch})
+
+#     for epoch, val_loss in enumerate(history.history['val_loss']):
+#         wandb.log({'val_loss': val_loss, "epoch": epoch})
+
+#     for epoch, val_acc in enumerate(history.history['val_accuracy']):
+#         wandb.log({'val_accuracy': val_acc, "epoch": epoch})
+
+#     print('Done Logging WandB metrics.')
+
 
 def display_results(cnn, loss, accuracy, f1, train_duration, val_duration):
     """ Print presentation info at process completion. """
@@ -53,6 +115,7 @@ def display_results(cnn, loss, accuracy, f1, train_duration, val_duration):
                                         "Time to Extract Features (sec)", "Videos/Second (Feat. Ext.)", \
                                         "Frames/Second (Feat. Ext.)"]))
     return
+
 
 def main():
     #parse args here
